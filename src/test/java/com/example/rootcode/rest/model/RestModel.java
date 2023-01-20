2d1267f69fc53d45a42bd5902c3c0cd2667abe07
Lakindu Widuranga Alwis <vidurangalakindu@gmail.com>
2023-01-20 12:42:50 +0530

    Add files via upload

---
+++ src/test/java/com/example/rootcode/rest/model/RestModel.java
@@ -0,0 +1,91 @@
+package com.example.rootcode.rest.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class RestModel {
+
+    //user
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String Email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+
+}
+
+
+@Entity
+@Table(name = "game")
+public class GameModel {
+
+    //game
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+
+    @Column(name = "description")
+    private String description;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
---
+++ src/test/java/com/example/rootcode/rest/repository/RestRepository.java
@@ -0,0 +1,8 @@
+package com.example.rootcode.rest.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RestRepository extends CrudRepository<RestModel, Integer> {
+}
+
+}
