1d994a69026504744eb883d1fbb0997cd30a7f46
Lakindu Widuranga Alwis <vidurangalakindu@gmail.com>
2023-01-20 12:42:18 +0530

    Delete rest directory

--- rest/src/test/java/com/example/rootcode/rest/repository/RestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.rootcode.rest.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RestRepository extends CrudRepository<RestModel, Integer> {
-}
-
-}
